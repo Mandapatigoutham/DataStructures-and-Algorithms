5e52036e7b20c1377ddcbad0ef44b7ca05f95196
Mandapati Goutham <121124621+Mandapatigoutham@users.noreply.github.com>
2024-03-01 15:03:37 +0530

    Problem Link
    https://www.codingninjas.com/studio/problems/childrensumproperty_790723

---
+++ Trees/39.Changing Tree into Children Sum Property.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if(!root)
+    return;
+    int ans=0;
+    if(root->left)
+    ans+=root->left->data;
+    if(root->right)
+    ans+=root->right->data;
+    if(ans<root->data)
+    {
+        if(root->left)
+        root->left->data=root->data;
+           if(root->right)
+        root->right->data=root->data;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+    int total=0;
+    if(root->left)
+    total+=root->left->data;
+    if(root->right)
+    total+=root->right->data;
+    if(root->left || root->right)
+    root->data=total;
+ 
+}  
