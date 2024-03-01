8f9c943c54034a67d39019b803a57195056000ba
Mandapati Goutham <121124621+Mandapatigoutham@users.noreply.github.com>
2024-03-01 14:21:41 +0530

    Problem Link
    https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ Trees/25.Right View Of Binary Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+         vector<int>ans;
+         fun(root,0,ans);
+         return ans;
+    }
+    void fun(TreeNode* root,int level,vector<int>&ans)
+    {
+        if(root==NULL)
+        return;
+        if(level==ans.size())
+        ans.push_back(root->val);
+        fun(root->right,level+1,ans);
+        fun(root->left,level+1,ans);
+    }
+};
