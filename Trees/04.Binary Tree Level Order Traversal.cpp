eed3167b41f41401a89e2eeaee7495fa3b03eb0e
Mandapati Goutham <121124621+Mandapatigoutham@users.noreply.github.com>
2024-03-01 13:37:25 +0530

    Problem Link
    https://leetcode.com/problems/binary-tree-level-order-traversal/

---
+++ Trees/04.Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)
+        return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            vector<int>temp;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left!=NULL)
+                q.push(node->left);
+                if(node->right!=NULL)
+                q.push(node->right);
+                temp.push_back(node->val);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
