d233ac9b4a8f9231d1d3ef436a12a426483126c5
Mandapati Goutham <121124621+Mandapatigoutham@users.noreply.github.com>
2024-03-02 00:31:40 +0530

    Problem Link
    https://www.geeksforgeeks.org/problems/inorder-traversal-and-bst5855/1

---
+++ Trees/58.Inorder Traversal and BST.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        // code here
+       for(int i=0;i<N-1;i++)
+       {
+           if(arr[i]>arr[i+1])
+           return 0;
+       }
+       return 1;
+    }
+};
